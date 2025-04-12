38dd4195ae55ac21bc818cec6fc0ba03179f628d
eugenekravchuk <jenia.kravchuk223@gmail.com>
2025-04-12 21:11:57 +0300

    added admin thing

---
+++ backend/locations/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Location, AccessibilityFeature, Review
+
+admin.site.register(Location)
+admin.site.register(AccessibilityFeature)
+admin.site.register(Review)
